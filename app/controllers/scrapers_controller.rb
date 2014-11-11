03f0a127be8550888c7079e277113d0db6ddd653
Daniel Burnette <dcburnette999@gmail.com>
2014-11-11 11:59:13 -0600

    undo previous failure

--- app/controllers/scrapers_controller.rb
+++ app/controllers/scrapers_controller.rb
@@ -13,11 +13,7 @@ class ScrapersController < ApplicationController
     new_start_url = params[:start_url] + '/tree/master/app/models'
     
     #Make sure URL is valid
-    if (new_start_url !=~ URI::regexp)
-      flash[:alert] = "You entered an invalid URL."
-      redirect_to :root
-      return 
-    end
+
     # Go through all directories recursively and grab the URLS for each file,
     # pushing them into @directory_urls
     scrape_all_urls(new_start_url)
