d37f98c79fc725036d1be86399f68caba5329fc4
Daniel Burnette <dcburnette999@gmail.com>
2014-12-22 13:49:23 -0600

    refactored some logic into transaction script - GetModelAssociations

--- app/controllers/scrapers_controller.rb
+++ app/controllers/scrapers_controller.rb
@@ -29,32 +29,10 @@ class ScrapersController < ApplicationController
     get_models_and_urls(@directory_urls)
 
     # Scrape each model page for their ActiveRecord assocations
-    @all_lines = []
+
     @all_relationships = []
     @model_urls.each do |url|
-      lines = Wombat.crawl do 
-        base_url url
-        data({css: ".js-file-line"}, :list)
-      end
-
-      lines = lines["data"]
-      @all_lines.push(lines)
-
-      relationships = []
-      #Filter out bad relationships
-      lines.each do |line|
-        line_split = line.split(' ')
-        #eliminating lines such as "attachment_fake_belongs_to_group(a)"
-        #23 is the length of has_and_belongs_to_many, the biggest relationship
-        if (line_split[0] != nil && line_split[0].length <= 23)
-          if (line_split[0].include?("belongs_to") || line_split[0].include?("has_one") ||
-              line_split[0].include?("has_many") || line_split[0].include?("belongs_to"))
-            if (!line.include?("validates") && !line.include?('#'))
-              relationships.push(line)
-            end
-          end 
-        end
-      end
+      relationships = GetModelAssociations.run(url)
       @all_relationships.push(relationships)
     end
 
