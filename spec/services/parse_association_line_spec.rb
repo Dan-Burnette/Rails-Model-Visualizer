6599975e381f7125584b625cbe8785c624e529af
Dan-Burnette <dcburnette999@gmail.com>
2020-05-25 15:49:24 -0500

    testing the namespacing detection stuff

--- spec/services/parse_association_line_spec.rb
+++ spec/services/parse_association_line_spec.rb
@@ -11,7 +11,7 @@ describe "ParseAssociationLine" do
 
     it "sets from_model properly" do
       line_double = double("line").as_null_object
-      association = ParseAssociationLine.call(@from_model, line_double)
+      association = ParseAssociationLine.call([], @from_model, line_double)
       expect(association.from_model).to eq(@from_model)
     end
 
@@ -19,19 +19,19 @@ describe "ParseAssociationLine" do
 
       it "parses type properly" do
         line = "some_association :project"
-        association = ParseAssociationLine.call(@from_model, line)
+        association = ParseAssociationLine.call([], @from_model, line)
         expect(association.type).to eq("some_association")
       end
 
       it "parses to_model properly" do
         line = "some_association :project"
-        association = ParseAssociationLine.call(@from_model, line)
+        association = ParseAssociationLine.call([], @from_model, line)
         expect(association.to_model).to eq("Project")
       end
 
       it "parses through_model properly" do
         line = "some_association :project"
-        association = ParseAssociationLine.call(@from_model, line)
+        association = ParseAssociationLine.call([], @from_model, line)
         expect(association.through_model).to eq(nil)
       end
 
@@ -41,13 +41,13 @@ describe "ParseAssociationLine" do
 
       it "parses to_model properly with hashrocket syntax" do
         line = "some_association :project, :class_name => 'Membership'"
-        association = ParseAssociationLine.call(@from_model, line)
+        association = ParseAssociationLine.call([], @from_model, line)
         expect(association.to_model).to eq("Membership")
       end
 
       it "parses to_model properly with hash syntax" do
         line = "some_association :project, class_name: 'Membership'"
-        association = ParseAssociationLine.call(@from_model, line)
+        association = ParseAssociationLine.call([], @from_model, line)
         expect(association.to_model).to eq("Membership")
       end
 
@@ -57,13 +57,13 @@ describe "ParseAssociationLine" do
 
       it "parses through_model properly with hashrocket syntax" do
         line = "some_association :members, :through => :group_projects"
-        association = ParseAssociationLine.call(@from_model, line)
+        association = ParseAssociationLine.call([], @from_model, line)
         expect(association.through_model).to eq("GroupProject")
       end
 
       it "parses through_model properly with hash syntax" do
         line = "some_association :members, through: :group_projects"
-        association = ParseAssociationLine.call(@from_model, line)
+        association = ParseAssociationLine.call([], @from_model, line)
         expect(association.through_model).to eq("GroupProject")
       end
 
@@ -71,16 +71,26 @@ describe "ParseAssociationLine" do
 
         it "parses to_model properly with hashrocket syntax" do
           line = "some_association :members, :through => :group_projects :source => :person"
-          association = ParseAssociationLine.call(@from_model, line)
+          association = ParseAssociationLine.call([], @from_model, line)
           expect(association.to_model).to eq("Person")
         end
 
         it "parses to_model properly with hash syntax" do
           line = "some_association :members, through: :group_projects, source: :person"
-          association = ParseAssociationLine.call(@from_model, line)
+          association = ParseAssociationLine.call([], @from_model, line)
           expect(association.to_model).to eq("Person")
         end
 
+      end
+
+    end
+
+    context "model and to_model are in the same namespace" do
+
+      it "parses a properly namespaced to_model" do
+        line = "some_association :members"
+        association = ParseAssociationLine.call(["Project::User", "Project::Member"], "Project::User", line)
+        expect(association.to_model).to eq("Project::Member")
       end
 
     end
