79e7577e2ec83e83a59c4b80110806fd3ae7c226
Dan-Burnette <dcburnette999@gmail.com>
2020-05-26 16:13:33 -0500

    back to using option terms seperately to prevent issues with models named similarly to an option (ie source)

--- services/parse_association_definition.rb
+++ services/parse_association_definition.rb
@@ -32,9 +32,11 @@ class ParseAssociationDefinition < ApplicationService
   end
 
   def to_model
-    if @line_terms.include?("class_name")
+    puts "option_keys are"
+    puts option_keys.inspect
+    if option_keys.include?("class_name")
       option("class_name").classify
-    elsif @line_terms.include?("source")
+    elsif option_keys.include?("source")
       option("source").classify
     elsif namespaced_to_model
       namespaced_to_model
@@ -48,19 +50,27 @@ class ParseAssociationDefinition < ApplicationService
   end
 
   def through_model
-    return nil if !@line_terms.include?("through")
-    through_term_index = @line_terms.index("through")
-    @line_terms[through_term_index + 1].classify
+    return nil if !option_terms.include?("through")
+    through_term_index = option_terms.index("through")
+    option_terms[through_term_index + 1].classify
   end
 
   def polymorphic?
-    return false if !@line_terms.include?("polymorphic")
+    return false if !option_terms.include?("polymorphic")
     option("polymorphic") == "true"
   end
 
+  def option_keys
+    option_terms.select.with_index { |t, i| i.even? }
+  end
+
+  def option_terms
+    @line_terms[2..-1]
+  end
+
   def option(name)
-    term_index = @line_terms.index(name)
-    @line_terms[term_index + 1]
+    term_index = option_terms.index(name)
+    option_terms[term_index + 1]
   end
 
   def namespaced_to_model
